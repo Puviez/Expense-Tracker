dc025b588486e23e8bcb673d563d17bcae3ae12a
Puviez <boredkeeper1793@gmail.com>
2019-11-19 00:55:02 +0800

    Edit User Functionality works

--- controllers/users.js
+++ controllers/users.js
@@ -2,10 +2,12 @@ const express = require('express')
 const users = express.Router()
 const User = require('../models/users.js')
 
+// New
 users.get('/new', (req, res) => {
   res.render('users/new.ejs')
 })
 
+// Create
 users.post('/', (req, res) => {
   User.create(req.body, (err, createdUser) => {
     if (err) {
@@ -15,5 +17,31 @@ users.post('/', (req, res) => {
     res.redirect('/')
   })
 })
+
+
+// Show
+users.get('/:id', (req, res) => {
+  User.findById(req.params.id, (err,user) => {
+      res.render('users/view.ejs', {
+          user: user
+      });
+  });
+});
+
+// Edit 
+users.get('/:id/edit', (req,res) => {
+  User.findById(req.params.id, (err,user) => {
+      res.render('users/edit.ejs', {
+          user: user
+      });
+  });
+});
+
+// Update
+users.put('/:id', (req,res) => {
+  User.findByIdAndUpdate(req.params.id, req.body, {new: true}, (err,updateUser) => {
+      res.redirect('/users/:id');
+  });
+});
 
 module.exports = users
