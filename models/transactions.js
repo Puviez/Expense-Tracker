0f15eb89d4b1087d29f1e436043a05792c54dc69
Puviez <boredkeeper1793@gmail.com>
2019-11-19 21:31:48 +0800

    Created Transaction Model

---
+++ models/transactions.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const transactionSchema = Schema({
+  date: {type: String, required: true},
+  details: {type: String, required: true},
+  classification: {type: String, required: true},
+  amount: {type: Number, required: true},
+  transaction_type: {type: String, required: true},
+  owner: { type: Schema.Types.ObjectId, ref: "userSchema" }
+})
+
+const Transaction = mongoose.model('Transcation', transactionSchema)
+
+module.exports = Transaction
